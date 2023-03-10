d993d3933b71eb4f022758209f55c7ffdbaa7846
Louise Kalfas <louise@kalfas.fr>
2023-03-10 12:52:50 +0100

    ajout mongoose - connection à la BD

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const userRouter = require('./routes/user');
+
+mongoose.connect(process.env.DATABASE_URL,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json());
 
