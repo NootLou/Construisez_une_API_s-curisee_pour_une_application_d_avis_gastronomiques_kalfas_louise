fae113d46a0a327cc38dd44764e962033947067c
Louise Kalfas <louise@kalfas.fr>
2023-03-10 15:48:26 +0100

    ajout route POST pour le compteur de like

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -64,3 +64,57 @@ exports.modifySauce = (req, res, next) => {
         })
         .catch(error => res.status(404).json({ error }));
 };
+
+exports.sauceLike = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            let userId = req.body.userId;
+            let like = req.body.like;
+            if (like === 1) {
+                // si userLiked ne contient pas l'id de lutilisateur, on ajoute son id au tableau et on augment de 1 la valeur de liked
+                if (!sauce.usersLiked.includes(userId)) {
+                    Sauce.updateOne({ _id: req.params.id }, {
+                        $push: { usersLiked: userId },
+                        $inc: { likes: +1 }
+                    })
+                        .then(() => res.status(200).json({ message: 'Vous aimez cette sauce !' }))
+                        .catch(error => res.status(400).json({ error }));
+                    //Sinon, si userLiked contient l'id de l'utilisateur, on renvoie une erreur avec un message
+                } else if (sauce.usersLiked.includes(userId)) {
+                    res.status(400).json({ message: 'Vous aimez déjà cette sauce !' })
+                };
+            } else if (like === 0) {
+                // si usersLiked contient l'id de l'utilisateur, on l'enlève et on enlève 1 au compteur de likes
+                if (sauce.usersLiked.includes(userId)) {
+                    Sauce.updateOne({ _id: req.params.id }, {
+                        $pull: { usersLiked: { $in: userId } },
+                        $inc: { likes: -1 },
+                    })
+                        .then(() => res.status(200).json({ message: "Vous n'aimez plus cette sauce." }))
+                        .catch(error => res.status(401))
+                    // sinon, si userDisliked contient l'id de l'utilisateur, on l'enlève et on enlève 1 au compteur de dislikes
+                } else if (sauce.usersDisliked.includes(userId)) {
+                    Sauce.updateOne({ _id: req.params.id }, {
+                        $pull: { usersDisliked: { $in: userId } },
+                        $inc: { dislikes: -1 },
+                    })
+                        .then(() => res.status(200).json({ message: "Cette sauce ne fait plus partie de celles que vous n'aimez pas." }))
+                        .catch(error => res.status(400).json({ error }));
+                }
+            } else if (like === -1) {
+                //Si l'usersDisliked ne contient pas l'id de l'utilisateur, on l'ajoute au tableau et on ajoute 1 au compteur de dislikes
+                if (!sauce.usersDisliked.includes(userId)) {
+                    Sauce.updateOne({ _id: req.params.id }, {
+                        $push: { usersDisliked: userId },
+                        $inc: { dislikes: +1 }
+                    })
+                        .then(() => res.status(200).json({ message: "Vous n'aimez pas cette sauce" }))
+                        .catch(error => res.status(400).json({ error }));
+                    //Sinon, si userLiked contient l'id de l'utilisateur, on renvoie une erreur avec un message
+                } else if (sauce.usersDisliked.includes(userId)) {
+                    res.status(400).json({ message: "Cette sauce fait déjà partie de celles que vous n'aimez pas" })
+                };
+            };
+        })
+        .catch(error => res.status(500).json({ error }))
+};
