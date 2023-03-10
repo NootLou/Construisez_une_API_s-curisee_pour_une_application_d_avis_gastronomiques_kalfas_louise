87b5eefb0495da2a25c4a7eefe636e7329510fb0
Louise Kalfas <louise@kalfas.fr>
2023-03-10 12:33:43 +0100

    ajout architecture des routes POST pour login

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
-
 const app = express();
 
+const userRouter = require('./routes/user');
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('api/auth', userRouter);
 
 
 module.exports = app;
