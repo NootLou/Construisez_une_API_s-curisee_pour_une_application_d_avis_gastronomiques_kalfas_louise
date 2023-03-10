46a6adc6c3dfd352ecd85d48b754f3ec791407a6
Louise Kalfas <louise@kalfas.fr>
2023-03-10 14:16:41 +0100

    correction token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -32,7 +32,7 @@ exports.login = (req, res, next) => {
                             token: jwt.sign(
                                 { userId: user._id },
                                 'CECI_EST_UN_TOKEN',
-                                { expiresIn: '24' },
+                                { expiresIn: '24h' },
                             )
                         })
                     ]
