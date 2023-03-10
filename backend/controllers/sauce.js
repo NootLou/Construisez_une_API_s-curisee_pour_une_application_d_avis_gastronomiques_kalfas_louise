9ed136c72421394ed6105f8448f8e4d8f925ceb5
Louise Kalfas <louise@kalfas.fr>
2023-03-10 15:23:19 +0100

    ajout de la route DELETE pour supprimer une sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require('../models/sauce');
+const fs = require('fs');
 
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
@@ -24,4 +25,21 @@ exports.createSauce = (req, res, next) => {
     sauce.save()
         .then(() => res.status(201).json({ message: 'Sauce créée !' }))
         .catch(error => res.status(400).json({ error }))
+};
+
+exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+            if (sauce.userId != req.auth.userId) {
+                res.status(403).json({ message: 'Requête non-autorisée' })
+            } else {
+                const filename = sauce.imageUrl.split('/images/')[1];
+                fs.unlink(`images/${filename}`, () => {
+                    Sauce.deleteOne({ _id: req.params.id })
+                        .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+                        .catch(error => res.status(401).json({ error }));
+                })
+            };
+        })
+        .catch(error => res.status(500).json({ error }));
 };
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -8,5 +8,6 @@ const sauceController = require('../controllers/sauce');
 router.get('/', auth, sauceController.getAllSauces);
 router.get('/:id', auth, sauceController.getOneSauce);
 router.post('/', auth, multer, sauceController.createSauce);
+router.delete('/:id', auth, multer, sauceController.deleteSauce);
 
 module.exports = router;
