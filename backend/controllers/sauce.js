2c0f52be5b32da8e0ef6cb1f275eba15d66129ce
Louise Kalfas <louise@kalfas.fr>
2023-03-10 14:07:13 +0100

    ajout route GET pour récupération d'une sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -2,6 +2,12 @@ const Sauce = require('../models/sauce');
 
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
-        .then( sauces => res.status(200).json( sauces))
+        .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(500).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
 };
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const sauceController = require('../controllers/sauce');
 
 router.get('/', sauceController.getAllSauces);
+router.get('/:id', sauceController.getOneSauce);
 
 module.exports = router;
