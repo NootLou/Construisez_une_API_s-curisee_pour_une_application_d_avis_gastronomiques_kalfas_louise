6d471619fa5b5c14a46fd6430b75ff46d0713559
Louise Kalfas <louise@kalfas.fr>
2023-03-10 14:12:34 +0100

    correction - ajout fichier auth.js

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth')
 
 const sauceController = require('../controllers/sauce');
 
-router.get('/', sauceController.getAllSauces);
-router.get('/:id', sauceController.getOneSauce);
+router.get('/', auth, sauceController.getAllSauces);
+router.get('/:id', auth, sauceController.getOneSauce);
 
 module.exports = router;
